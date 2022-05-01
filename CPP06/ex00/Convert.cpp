96f2daeec986acefb54fa0314326e5bcee283897
Dasom Im <dim@student.42seoul.kr>
2022-05-01 23:10:04 +0900

    ING: cpp06

---
+++ CPP06/ex00/Convert.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Convert.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dim <dim@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/01 20:22:41 by dim               #+#    #+#             */
+/*   Updated: 2022/05/01 21:55:08 by dim              ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Convert.hpp"
+
+Convert::Convert()
+: input(""), isNum(ture)
+{}
+
+Convert::Convert(const std::string& input) {
+
+}
+
+Convert::Convert(const Convert& other) {}
+
+Convert& Convert::operator=(const Convert& rhs) {}
+
+Convert::~Convert() {}
+
---
+++ CPP06/ex00/Convert.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Convert.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dim <dim@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/01 20:19:53 by dim               #+#    #+#             */
+/*   Updated: 2022/05/01 21:14:06 by dim              ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef _CONVERT_HPP__
+#define _CONVERT_HPP__
+#include <iostream>
+#include <string>
+
+class Convert
+{
+private:
+	std::string	input;
+	bool	isNum;
+public:
+	Convert();
+	Convert(const std::string& input);
+	Convert(const Convert& other);
+	Convert& operator=(const Convert& rhs);
+	~Convert();
+};
+
+#endif
